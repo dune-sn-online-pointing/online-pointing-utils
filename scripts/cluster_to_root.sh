d836d56cd9f1bb4c2710ccab192abd7a4397cb1b
Dariogon1998 <dariopullia@yahoo.it>
2024-02-26 18:41:35 +0100

    scripts fixed

--- scripts/cluster_to_root.sh
+++ scripts/cluster_to_root.sh
@@ -10,7 +10,7 @@ MAX_EVENTS_PER_FILENAME=1000 #
 ADC_INTEGRAL_CUT=0 # if 0, no cut
 # compile the code
 
-REPO_HOME='git rev-parse --show-toplevel'
+REPO_HOME=$(git rev-parse --show-toplevel)
 echo "REPO_HOME: ${REPO_HOME}"
 
 cd ${REPO_HOME}/build
@@ -24,7 +24,7 @@ fi
 
 # TODO handle all these trough json
 input_file=/afs/cern.ch/work/d/dapullia/public/dune/online-pointing-utils/lists/text_files/cc_lab.txt
-output_folder=/eos/user/d/dapullia/root_cluster_files/eliminami/
+output_folder=/eos/user/d/dapullia/dune/root_cluster_files/cc_lab/
 
 # already in build
 execution_command="./app/cluster_to_root -f $input_file -o $output_folder --ticks-limit $TICK_LIMITS --channel-limit $CHANNEL_LIMIT --min-tps-to-cluster $MIN_TPS_TO_CLUSTER --plane $PLANE --supernova-option $SUPERNOVA_OPTION --main-track-option $MAIN_TRACK_OPTION --max-events-per-filename $MAX_EVENTS_PER_FILENAME --adc-integral-cut $ADC_INTEGRAL_CUT"
