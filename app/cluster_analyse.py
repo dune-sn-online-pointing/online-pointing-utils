1e588375e6d465716bf3a2609541838b668d99b1
Henry Benjamin Akins <hakins@lxplus9100.cern.ch>
2024-02-29 16:35:29 +0100

    Sync changes with Emanuele

--- app/cluster_analyse.py
+++ app/cluster_analyse.py
@@ -29,19 +29,19 @@ from cluster import read_root_file_to_clusters
 parser = argparse.ArgumentParser(description='Tranforms Trigger Primitives to images.')
 parser.add_argument('-i', '--input-file',           type=str,                         help='Input file name')
 parser.add_argument('-o', '--output-path',          type=str,    default='./',        help='path to save the image')
-parser.add_argument('-n', '--n-tps',                type=int,    default=1,           help='number of tps to process')
+parser.add_argument('-n', '--n-tps',                type=int,    default=100,           help='number of tps to process')
 parser.add_argument('-t', '--ticks-limit',          type=int,    default=3,           help='closeness in ticks to cluster TPs')
 parser.add_argument('-c', '--channel-limit',        type=int,    default=1,           help='closeness in channels to cluster TPs')
 parser.add_argument('-m', '--min-tps',              type=int,    default=3,           help='minimum number of TPs to create a cluster and then an image')
 parser.add_argument('--channel-map',                type=str,    default="APA",       help='"APA", "CRP" or "50L"')
-parser.add_argument('--save-clusters',                action='store_true',              help='write the clusters to a textfile')
-parser.add_argument('--show',                       action='store_true',              help='show the image')
-parser.add_argument('--fixed-size',                 action='store_true',              help='make the image size fixed')
+parser.add_argument('--save-clusters',                action='store_true',     default=True,          help='write the clusters to a textfile')
+parser.add_argument('--show',                       action='store_true',     default=True,          help='show the image')
+parser.add_argument('--fixed-size',                 action='store_true',     default=True,          help='make the image size fixed')
 parser.add_argument('--img-width',                  type=int,    default=200,          help='width of the image, if fixed size')
 parser.add_argument('--img-height',                 type=int,    default=300,         help='height of the image, if fixed size')
 parser.add_argument('--x-margin',                   type=int,    default=2,           help='margin in x')
 parser.add_argument('--y-margin',                   type=int,    default=2,          help='margin in y')
-parser.add_argument('-v', '--verbose',              action='store_true',              help='verbose mode')
+parser.add_argument('-v', '--verbose',              action='store_true', default=True,             help='verbose mode')
 
 args = parser.parse_args()
 input_file              = args.input_file
@@ -84,36 +84,45 @@ print (" ")
 # reading with this function already orders by time_start
 # all_TPs = save_tps_array(input_file, max_tps=n_tps) 
 
-if verbose:
-    print("TPs read from file: ", input_file)
-    print("Number of TPs: ", len(all_TPs))
-    print(" ")
+# if verbose:
+#     print("TPs read from file: ", input_file)
+#     print("Number of TPs: ", len(all_TPs))
+#     print(" ")
 
 # create channel map to distinguish induction and collection
 # my_channel_map = create_channel_map_array(which_channel_map=channel_map)
 
-clusters, n_events = read_root_file_to_clusters('/afs/cern.ch/work/h/hakins/private/online-pointing-utils/scripts/output/X/clusters_tick_limits_3_channel_limits_1_min_tps_to_cluster_1.root')
+clusters, n_events = read_root_file_to_clusters('/afs/cern.ch/work/h/hakins/private/online-pointing-utils/scripts/output/X/background_file_CC_smaller.root')
 
 print("Number of clusters: ", len(clusters))
-print("First cluster entry: " + str(clusters[0]))
+#print("First cluster entry: " + str(clusters[0]))
 
 total_charges = [] #array of total charges per cluster
+true_labels = []
 
 for cluster in clusters:
     charge_sum = 0
-    for event in cluster:
-        charge_sum = charge_sum + event[4]
+
+    for tp in cluster.tps_:
+        charge_sum = charge_sum + tp['adc_integral']
+    
+    true_labels.append(cluster.true_label_)
 
     total_charges.append(charge_sum)
 
-print("total charge per cluster: " + str(total_charges))
+#print("total charge per cluster: " + str(total_charges))
+plt.figure
 
-plt.hist(total_charges, bins=5, edgecolor='black')  
-plt.xlabel('Total Charge per cluster [ADC]')
-plt.ylabel('Number of Clusters')
-plt.title('')
-plt.grid(False)
+# plt.hist(total_charges, bins=5, edgecolor='black')  
+# plt.xlabel('Total Charge per cluster [ADC]')
+# plt.ylabel('Number of Clusters')
+# plt.title('')
+# plt.grid(False)
 
-plt.savefig('plots/histogram.png')
 
+# plt.savefig('plots/charges.png')
+
+plt.figure
+plt.hist(true_labels)
+plt.savefig('plots/true_labels.png')
 #copy plot to local: scp hakins@lxplus.cern.ch:/afs/cern.ch/user/h/hakins/tpgtools/scripts/plots/histogram.png C:\Users\harry\Documents\Test_Plots
