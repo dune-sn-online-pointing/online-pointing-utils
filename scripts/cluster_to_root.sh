d1c6edac778be404f1337ffa1e3140fa717106c8
evila <emanuele.villa@cern.ch>
2024-10-01 15:53:12 +0200

    cluster to root finished

--- scripts/cluster_to_root.sh
+++ scripts/cluster_to_root.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-### !!!!! THIS IS FROM HARRY, SPECIAL APPLICATION
+### !!!!! THIS IS FROM HARRY, SPECIAL APPLICATION IS COMMENTED
 
 REPO_HOME=$(git rev-parse --show-toplevel)
 current_dir=$(pwd)
 
 print_help() {
     echo "************************************************************************************************"
-    echo "Usage: ./cluster_to_root.sh --cut <cut> --type <type> [Either "main_track" "blip" "benchmark "or "bkg"]"
+    echo "Usage: ./cluster_to_root.sh -j <json_settings.json>" #--cut <cut> --type <type> [Either "main_track" "blip" "benchmark "or "bkg"]"
     echo "Options:"
-    echo "  --cut <cut>                           Cut value"
-    echo "  --type <type>                         Type of the dataset, either 'main_track', 'blip', 'bkg', 'benchmark'"
+    echo "  --json-settings <json>                Json file to used as input. Relative path inside json/"
+    # echo "  --cut <cut>                           Cut value"
+    # echo "  --type <type>                         Type of the dataset, either 'main_track', 'blip', 'bkg', 'benchmark'"
     echo "  -h, --help                            Print this help message"
     echo "************************************************************************************************"
     exit 0
@@ -20,20 +21,35 @@ print_help() {
 while [[ $# -gt 0 ]]; do
     case "$1" in
         --output-folder) output_folder="$2"; shift 2 ;;
-        --cut)          cut="$2"; shift 2 ;;
-        --type)         type="$2"; shift 2 ;;
+        -j|--json-settings) INPUT_JSON="$2"; shift 2 ;;
+        # --cut)          cut="$2"; shift 2 ;;
+        # --type)         type="$2"; shift 2 ;;
         -h|--help)      print_help ;;
         *)              shift ;;
     esac
 done
 
-echo "REPO_HOME: ${REPO_HOME}"
-json_settings_file="${output_folder}/cluster_to_root/${type}/${type}_${cut}.json"
-echo "Json file to be created: ${json_settings_file}"
+# check if json was selected
+if [ -z "$INPUT_JSON" ]; then
+    echo "Json settings file not provided"
+    exit 1
+fi
 
-echo "Creating json config from the information provided..."
-python ${REPO_HOME}/python/cluster_to_root_json_creator.py --cut "$cut" --output_file "$json_settings_file" --type "$type" 
-echo "Json settings file created at ${json_settings_file}"
+# check if json file exists 
+if [ ! -f "$INPUT_JSON" ]; then
+    echo "Json settings file does not exist"
+    exit 1
+fi
+
+
+echo "REPO_HOME: ${REPO_HOME}"
+
+# json_settings_file="${output_folder}/cluster_to_root/${type}/${type}_${cut}.json"
+# echo "Json file to be created: ${json_settings_file}"
+
+# echo "Creating json config from the information provided..."
+# python ${REPO_HOME}/python/cluster_to_root_json_creator.py --cut "$cut" --output_file "$json_settings_file" --type "$type" 
+# echo "Json settings file created at ${json_settings_file}"
 
 # compile
 echo "Compiling..."
@@ -47,10 +63,13 @@ if [ $? -ne 0 ]; then
 fi
 
 # Run the app
-# TODO still fix this
 # ES
-INPUT_JSON="/afs/cern.ch/work/d/dapullia/public/dune/online-pointing-utils/json/cluster_to_root/es_lab.json"
-./app/cluster_to_root -j $INPUT_JSON
+# INPUT_JSON="/afs/cern.ch/work/d/dapullia/public/dune/online-pointing-utils/json/cluster_to_root/es_lab.json"
+
+command_to_run="./app/cluster_to_root -j $INPUT_JSON"
+echo "Running command: ${command_to_run}"
+eval $command_to_run
+
 # CC
 # INPUT_JSON="/afs/cern.ch/work/d/dapullia/public/dune/online-pointing-utils/json/cluster_to_root/cc_lab.json"
 # ./app/cluster_to_root -j $INPUT_JSON
