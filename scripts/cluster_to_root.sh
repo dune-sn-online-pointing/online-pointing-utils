3fe3a0329708be4caa81f7d9ff5d56c30416c448
evila <emanuele.villa@cern.ch>
2024-10-01 15:33:22 +0200

    reorganizing scripts/, in the process. Deleted useless things

--- scripts/cluster_to_root.sh
+++ scripts/cluster_to_root.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+### !!!!! THIS IS FROM HARRY, SPECIAL APPLICATION
+
 REPO_HOME=$(git rev-parse --show-toplevel)
+current_dir=$(pwd)
 
 print_help() {
     echo "************************************************************************************************"
@@ -8,7 +11,7 @@ print_help() {
     echo "Options:"
     echo "  --cut <cut>                           Cut value"
     echo "  --type <type>                         Type of the dataset, either 'main_track', 'blip', 'bkg', 'benchmark'"
-    echo "  -h, --help                           Print this help message"
+    echo "  -h, --help                            Print this help message"
     echo "************************************************************************************************"
     exit 0
 }
@@ -24,10 +27,14 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-output_file="/afs/cern.ch/work/h/hakins/private/json/cluster_to_root/${type}/${type}_${cut}.json"
-cd /afs/cern.ch/work/h/hakins/private/online-pointing-utils/scripts/json_creators
-python cluster_to_root_json_creator.py --cut "$cut" --output_file "$output_file" --type "$type" 
 echo "REPO_HOME: ${REPO_HOME}"
+json_settings_file="${output_folder}/cluster_to_root/${type}/${type}_${cut}.json"
+echo "Json file to be created: ${json_settings_file}"
+
+echo "Creating json config from the information provided..."
+python ${REPO_HOME}/python/cluster_to_root_json_creator.py --cut "$cut" --output_file "$json_settings_file" --type "$type" 
+echo "Json settings file created at ${json_settings_file}"
+
 # compile
 echo "Compiling..."
 cd ${REPO_HOME}/build/
@@ -38,18 +45,14 @@ if [ $? -ne 0 ]; then
     echo "Compilation failed"
     exit 1
 fi
-echo "INput Json: ${output_file}"
-# Run the app
-# INPUT_JSON="/afs/cern.ch/work/d/dapullia/public/dune/online-pointing-utils/json/cluster_to_root/main_tracks.json"
-# ./app/cluster_to_root -j $INPUT_JSON
-# INPUT_JSON="/afs/cern.ch/work/d/dapullia/public/dune/online-pointing-utils/json/cluster_to_root/blips.json"
-# ./app/cluster_to_root -j $INPUT_JSON
-# INPUT_JSON="/afs/cern.ch/work/d/dapullia/public/dune/online-pointing-utils/json/cluster_to_root/backgrounds.json"
-# ./app/cluster_to_root -j $INPUT_JSON
 
+# Run the app
+# TODO still fix this
+# ES
 INPUT_JSON="/afs/cern.ch/work/d/dapullia/public/dune/online-pointing-utils/json/cluster_to_root/es_lab.json"
 ./app/cluster_to_root -j $INPUT_JSON
-INPUT_JSON="/afs/cern.ch/work/d/dapullia/public/dune/online-pointing-utils/json/cluster_to_root/cc_lab.json"
-./app/cluster_to_root -j $INPUT_JSON
+# CC
+# INPUT_JSON="/afs/cern.ch/work/d/dapullia/public/dune/online-pointing-utils/json/cluster_to_root/cc_lab.json"
+# ./app/cluster_to_root -j $INPUT_JSON
 
-cd ${REPO_HOME}/scripts/
+cd ${current_dir}
