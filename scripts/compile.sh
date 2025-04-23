5cc80b50699bdac57f6baba4a9f167e1c7733708
Emanuele Villa <villa.97@live.it>
2025-04-23 17:07:35 -0500

    added init.sh, compile.sh, findSettings.sh, and refurbished cluster_to_root with these new ones

---
+++ scripts/compile.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Script to compile the code, stopping in case of error
+
+echo "***Script compile.sh***"
+
+# Initialize env variables
+export SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source $SCRIPTS_DIR/init.sh
+
+# Initialize variables
+cleanCompile=false
+noCompile=false
+
+# Function to print help message
+print_help() {
+    echo "*****************************************************************************"
+    echo "Usage: ./compile.sh [-p <home_path>] [--clean-compile <true, false>] [--no-compile <true, false>] [-h]"
+    echo "  -p | --home-path      path to the code home directory (/your/path/tof-reco, no / at the end). There is no default."
+    echo "  -n | --no-compile          do not recompile the code. Default is to recompile the code"
+    echo "  -c | --clean-compile       clean and recompile the code. Default is to recompile the code without cleaning"
+    echo "  -h | --help           print this help message"
+    echo "*****************************************************************************"
+    exit 0
+}
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    -p|--home-path)         HOME_DIR="$2"; echo " Got home path as argument";    shift 2 ;;
+    -n|--no-compile)        noCompile="${2:-true}";      shift ;;
+    -c|--clean-compile)     cleanCompile="${2:-true}";   shift ;;
+    -h|--help)              print_help ;;
+    *)                      shift ;;
+    esac
+done
+
+# if HOME_DIR is empty, print help message and exit
+if [ -z "$HOME_DIR" ]; then
+    echo "  Error: HOME_DIR is empty, meaning that you have not run from the repo and not given it as argument to the script."
+    echo "  Please provide the path to the code home directory."
+    print_help
+fi
+
+# if no compile, close the script
+if [ "$noCompile" == true ]; then
+    echo "  No compilation was requested. Stopping execution of this script."
+    echo ""
+else
+    echo "  Compiling the code, these steps need to be run from build/"
+    mkdir -p $BUILD_DIR || exit # in case it does not exist
+    cd $BUILD_DIR || exit
+
+    if [ "$cleanCompile" == true ]; then  
+        echo "  Cleaning build folder..."
+        rm -rfv $BUILD_DIR/*
+        echo "  Done!"
+    fi
+
+    cmake ..
+    # check if cmake was successful
+    if [ $? -ne 0 ]
+    then
+        echo "  CMake failed. Stopping execution."
+        echo ""
+        exit 0
+    fi
+    nproc=$(nproc)
+
+    make -j $nproc
+    # check if make was successful
+    if [ $? -ne 0 ]
+    then
+        echo "  Make failed. Stopping execution."
+        echo ""
+        exit 0
+    fi
+
+    echo "  Compilation successful!"
+    echo ""
+fi
---
+++ scripts/init.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [[ $INIT_DONE == true ]]; then
+    echo "Environment is already initialized, not running init.sh again"
+else
+
+    echo ""
+    echo "init.sh script started"
+
+    export SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    export HOME_DIR=$(dirname $SCRIPTS_DIR)
+    echo " Repository home is $HOME_DIR"
+
+    # In lxplus, nothing special should be needed 
+    # In fnal, we need to be in a container and set up root
+
+    if [[ $(hostname) == *"lxplus"* ]]; then
+        echo " In lxplus, no need to source an environment or packages."
+    elif [[ $(hostname) == *"fnal"* ]]; then
+        if grep -q "Scientific Linux release 7" /etc/redhat-release 2>/dev/null; then
+            echo " Setting up environment for fnal cluster, using ups products. If this fails, it means you're not in a slf7 container."
+            # Set up the basic environment
+            source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+            echo " Setting up root, cmake..."
+            setup root v6_26_06a -q e26:p3913:prof # could be another, not too important
+            setup cmake  v3_27_4
+            echo " Done!"
+        else
+            echo " WARNING: not running in slf7, can't set up dune products and commands, can't compile."
+        fi
+    fi
+
+    export JSON_DIR=$HOME_DIR"/json/"
+    export PYTHON_DIR=$HOME_DIR"/python/"
+    export SRC_DIR=$HOME_DIR"/src/"
+    export BUILD_DIR=$HOME_DIR"/build/"
+
+    export INIT_DONE=true
+
+    echo "init.sh finished"
+    echo ""
+
+fi
