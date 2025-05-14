7d70fe2bc13510d46eb0a983ca1cd6ad8cfa6ed6
emanuele-villa <villa.97@live.it>
2025-05-14 09:48:12 -0500

    updates to init for python part

--- scripts/init.sh
+++ scripts/init.sh
@@ -21,10 +21,13 @@ else
             echo " Setting up environment for fnal cluster, using ups products. If this fails, it means you're not in a slf7 container."
             # Set up the basic environment
             source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
-            echo " Setting up root, cmake, valgrind..."
-            setup root v6_26_06a -q e26:p3913:prof # could be another, not too important
+            echo " Setting up necessary packages..."
+            # versions are chosen for compatibility with the pyenv
+            setup root v6_26_06a -q e20:p3913:prof
+            setup nlohmann_json v3_10_4_1 -q e26:prof
             setup cmake  v3_27_4
             setup valgrind v3_10_1
+            setup gcc v9_3_0
             echo " Done!"
         else
             echo " WARNING: not running in slf7, can't set up dune products and commands, can't compile."
@@ -35,6 +38,14 @@ else
     export PYTHON_DIR=$HOME_DIR"/python/"
     export SRC_DIR=$HOME_DIR"/src/"
     export BUILD_DIR=$HOME_DIR"/build/"
+
+    # activate the python environment
+    if [[ -d $PYTHON_DIR/online-pointing-env ]]; then
+        echo " Activating python environment"
+        source $PYTHON_DIR/online-pointing-env/bin/activate
+    else
+        echo " WARNING: python environment not found, not activating."
+    fi
 
     export INIT_DONE=true
 
