0026114b0ecb7e021e71c59cf389275941475be6
emanuele-villa <villa.97@live.it>
2025-05-14 09:47:04 -0500

    added generic toolbox submdodule

--- cmake/submodules.cmake
+++ cmake/submodules.cmake
@@ -41,3 +41,14 @@ add_definitions( -D LOGGER_ENABLE_COLORS=1 )
 add_definitions( -D LOGGER_ENABLE_COLORS_ON_USER_HEADER=1 )
 #add_definitions( -D LOGGER_PREFIX_FORMAT="\\\"{TIME} {USER_HEADER} {FILELINE}"\\\" )
 add_definitions( -D LOGGER_PREFIX_FORMAT="\\\"{TIME} {USER_HEADER}"\\\" )
+
+
+###################################
+# C++ generic toolbox
+###################################
+cmessage( STATUS "Looking for cpp-generic-toolbox submodule..." )
+checkSubmodule( cpp-generic-toolbox )
+include_directories( ${SUBMODULE_DIR}/cpp-generic-toolbox/include )
+add_definitions( -D PROGRESS_BAR_FILL_TAG="\\\"SNOP\#"\\\" )
+add_definitions( -D PROGRESS_BAR_ENABLE_RAINBOW=1 )
+
